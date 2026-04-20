5d2ceaf89c0fc4b6163288bd12cd930df3166db0
Tirth Sevak <Tsevak4276@conestogac.on.ca>
2026-04-20 19:20:54 -0400

    feat: add delete post endpoint

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, getPosts, updatePost } from "../controllers/PostController";
+import { createPost, getPosts, updatePost, deletePost } from "../controllers/PostController";
 
 const router = express.Router();
 
 router.post("/", createPost);
 router.get("/", getPosts);
 router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
