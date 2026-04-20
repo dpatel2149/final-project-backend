0c76e961c9f972e1ee2fe03414df115bb19d2162
Tirth Sevak <Tsevak4276@conestogac.on.ca>
2026-04-20 19:04:46 -0400

    feat: add update post endpoint

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/PostController";
+import { createPost, getPosts, updatePost } from "../controllers/PostController";
 
 const router = express.Router();
 
 router.post("/", createPost);
 router.get("/", getPosts);
+router.put("/:id", updatePost);
 
 export default router;
